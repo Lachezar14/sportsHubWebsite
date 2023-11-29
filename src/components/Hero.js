0ffef83bebabc82efbc832966615b9daf5fa262f
Lachezar Mitov <lachezarspam1@gmail.com>
2023-11-29 14:57:54 +0100

    Fix signing up to waitlist. Add alerts

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,77 @@
-﻿export default function Hero() {
+﻿import supabase from "../supabaseClient";
+import {useEffect, useState} from "react";
+import {IconCheck, IconUserPlus} from "@tabler/icons-react";
+
+export default function Hero() {
+
+    const [email, setEmail] = useState(null);
+    const [success, setSuccess] = useState(false);
+    const [error, setError] = useState(false);
+    const [users, setUsers] = useState([]);
+    const [errorText, setErrorText] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                let { data: usersData, error } = await supabase
+                    .from('users')
+                    .select('*');
+                if (!error) {
+                    setUsers(usersData);
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    
+    const isEmailAlreadySignedUp = (email) => {
+        return users.some((user) => user.email === email);
+    };
+
+    const isValidEmail = (email) => {
+        // Regular expression for a simple email validation
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    };
+
+    const signUp = async (email) => {
+        
+        if (!isValidEmail(email)) {
+            setError(true);
+            setErrorText("Please enter a valid email address.");
+            return;
+        }
+
+        if (isEmailAlreadySignedUp(email)) {
+            setError(true);
+            setErrorText("You have already signed up.");
+            return;
+        }
+        
+
+        try {
+            const {data, error} = await supabase
+                .from('users')
+                .insert([
+                    {email: email},
+                ])
+                .select()
+
+            if (!error) {
+                setError(false)
+                setSuccess(true);
+                setTimeout(() => {
+                    setSuccess(false);
+                }, 4000);
+            }
+        } catch (error) {
+            console.error("Error signing up:", error);
+        }
+    }
 
     return (
         <div className="ml-44 mr-24 mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
@@ -11,11 +84,33 @@
                     Sign up below to get the SportsHub app and be the first to experience the new way of experiencing sports events.
                 </p>
                 <div className="flex justify-center">
-                    <button className="inline-flex items-center px-5 py-3 mt-2 font-medium text-white transition duration-500 ease-in-out transform bg-transparent rounded-lg bg-gray-900"
-                    >
-                        Find out more
+                    <input
+                        type="email"
+                        placeholder="jack@example.com"
+                        name="email"
+                        autoComplete="email"
+                        className="border border-gray-500 pr-28 pl-2 py-3 mt-2 rounded-md text-gray-900 font-semibold hover:border-gray-700 bg-white"
+                        onChange={(e) => setEmail(e.target.value)}
+                    />{" "}
+                    <button
+                        className="inline-flex items-center px-8 py-3 mt-2 ml-2 font-medium text-white transition duration-500 ease-in-out transform bg-transparent border border-black rounded-lg bg-black"
+                        onClick={() => signUp(email)}>
+                        Join the waitlist
                     </button>
                 </div>
+                {success ? <div className="bg-teal text-center py-4 lg:px-4">
+                    <div className="pr-20 pl-10 pb-1 pt-1 ml-12 bg-green-700 items-center text-white leading-none lg:rounded-full flex lg:inline-flex" role="alert">
+                        <div className="flex rounded-full uppercase px-2 py-1 text-xs font-bold mr-3">
+                            <IconCheck width="20" height="20" strokeWidth={2} color="white"/>
+                        </div>
+                        <span className="font-semibold mr-2 text-white flex-auto">Thanks for signing up!</span>
+                    </div>
+                </div> : <p></p>}
+                {error ? <div className="bg-teal text-center py-4 lg:px-4">
+                    <div className="pr-20 pl-10 pb-1 pt-1 ml-12 bg-teal items-center text-white leading-none lg:rounded-full flex lg:inline-flex" role="alert">
+                        <span className="font-semibold mr-2 text-red-900 flex-auto">{errorText}</span>
+                    </div>
+                </div> : <p></p>}
             </div>
             <div className="xl:mr-48 ml-24 sm:mr-0 sm:mb-28 mb-0 lg:mb-0 mr-48 md:pl-10">
                 <img
--- src/components/Main.js
+++ src/components/Main.js
@@ -32,15 +32,28 @@ export default function Main() {
 
         fetchData();
     }, []);
-    
+
+    const isValidEmail = (email) => {
+        // Regular expression for a simple email validation
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    };
+
     const signUp = async (email) => {
+
+        if (!isValidEmail(email)) {
+            return;
+        }
+
+
         try {
             const {data, error} = await supabase
                 .from('users')
                 .insert([
-                    {first_name:"Bily", last_name:"Joe", email: email},
+                    {email: email},
                 ])
                 .select()
+            
         } catch (error) {
             console.error("Error signing up:", error);
         }
@@ -58,8 +71,7 @@ export default function Main() {
                 <div className="max-w-7xl bg-black rounded-3xl mx-auto px-4 mb-44 sm:px-6 text-center">
                     <div className="py-24 md:py-24">
                         <div className="mx-auto max-w-screen-md sm:text-center">
-                            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-white">Sign
-                                up to get the app!</h2>
+                            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-white">Join the waitlist to get the app!</h2>
                             <p className="mx-auto mb-8 max-w-2xl font-light text-gray-300 md:mb-12 sm:text-xl dark:text-gray-400">
                                 If you like Sports Hub and want to try it 
                                 sign up with your email to be first to get notified when the app is live!</p>
@@ -71,27 +83,28 @@ export default function Main() {
                                             address</label>
                                         <div
                                             className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                                            <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
+                                            {/*<svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
                                                  viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                                 <path
                                                     d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
                                                 <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
-                                            </svg>
+                                            </svg>*/}
                                         </div>
+                                    </div>
+                                        {/*
                                         <input
                                             className="block p-3 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:rounded-none sm:rounded-l-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                             placeholder="Enter your email" type="email" id="email" required=""
                                             onChange={(e) => setEmail(e.target.value)}
                                         >
                                         </input>
-                                    </div>
                                     <div>
                                         <button 
-                                            className="py-3 px-5 w-full text-sm font-medium text-center text-black rounded-lg border cursor-pointer bg-teal border-teal sm:rounded-none sm:rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                                            onClick={handleClick}>
-                                                Subscribe
+                                            className="py-3 px-5 w-full  text-sm font-medium  text-black rounded-lg border cursor-pointer bg-teal border-teal sm:rounded-none sm:rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                                            onClick={() => signUp(email)}>
+                                                Join 
                                         </button>
-                                    </div>
+                                    </div>*/}
                                 </div>
                             </form>
                         </div>
